ca3a3ea6d1e775cba4ac16c094bbc7f1dc58d3af
Vladislav <vladislavforeverstepanov@yandex.ru>
2024-06-26 20:43:03 +0300

    Add changes to tests.py and README.md

--- tests.py
+++ tests.py
@@ -1,24 +1,75 @@
+import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
-class TestBooksCollector:
+class TestBookCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    def test_add_new_book(self):
+        book = BooksCollector()
+        book.add_new_book('Буратино')
+        assert len(book.get_books_genre()) == 1
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+class TestBookCollector:
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_set_book_genre_check_book_in_books_genre(self):
+       book = BooksCollector()
+       book.add_new_book('Буратино')
+       book.set_book_genre('Буратино', 'Мультфильмы')
+       assert 'Буратино' in book.books_genre == {'Буратино': 'Мультфильмы'}
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+class TestBookCollector:
+    @pytest.mark.parametrize('genre', ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии'])
+    def test_set_book_genre_check_in_genre(self, genre):
+
+        book = BooksCollector()
+        book.add_new_book('Буратино')
+        book.set_book_genre('Буратино', 'Мультфильмы')
+        assert 'Мультфильмы' in book.genre == book.genre
+
+class TestBookCollector:
+
+    def test_get_book_genre(self):
+        book = BooksCollector()
+        book.add_new_book('Буратино')
+        book.set_book_genre('Буратино', 'Мультфильмы')
+        assert book.books_genre.get('Буратино') == 'Мультфильмы'
+
+class TestBookCollector:
+
+    def test_get_books_with_specific_genre(self):
+        book = BooksCollector()
+        book.add_new_book('Буратино')
+        book.set_book_genre('Буратино', 'Мультфильмы')
+        assert book.get_books_with_specific_genre('Мультфильмы') == ['Буратино']
+
+class TestBookCollector:
+
+    def test_get_books_genre(self):
+        book = BooksCollector()
+        book.add_new_book('Буратино')
+        book.set_book_genre('Буратино', 'Мультильмы')
+        book.books_genre['Буратино'] = book.genre
+        assert book.get_books_genre() == {'Буратино': ''}
+
+class TestBookCollector:
+    @pytest.mark.parametrize('genre_age_rating', ['Ужасы', 'Детективы'])
+    def test_get_books_for_children(self, genre_age_rating):
+        book = BooksCollector()
+        book.add_new_book('Буратино')
+        book.set_book_genre('Буратино', 'Мультфильмы')
+        assert book.genre not in book.genre_age_rating == book.genre_age_rating
+
+class TestBookCollector:
+
+   def test_add_book_in_favorites(self):
+        book = BooksCollector()
+        book.add_new_book('Буратино')
+        book.add_book_in_favorites('Буратино')
+        assert len(book.get_list_of_favorites_books()) == 1
+
+class TestBookCollector:
+
+    def test_delete_book_from_favorites(self):
+        book = BooksCollector()
+        book.add_book_in_favorites('Буратино')
+        book.delete_book_from_favorites('Буратино')
+        assert len(book.get_list_of_favorites_books()) == 0
